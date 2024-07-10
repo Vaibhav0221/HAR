2ae461446719aefb4bbf0fc48459d722a5922510
Vaibhav0221 <138066795+Vaibhav0221@users.noreply.github.com>
2024-07-11 01:56:14 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request
+import numpy as np
+from PIL import Image
+from keras.models import load_model
+
+app = Flask(__name__)
+path_to_model = '/Users/vaibhav/Desktop/HAR/Flask/static/Models/Model2.h5'
+V_model = load_model(path_to_model)
+
+def read_image(fn):
+    image = Image.open(fn)
+    return np.asarray(image.resize((224, 224)))
+
+def test_predict(test_image):
+    result = V_model.predict(np.asarray([read_image(test_image)]))
+    itemindex = np.where(result == np.max(result))
+    prediction = itemindex[1][0]
+    label_map = {
+        0: "Sitting",
+        1: "Drinking",
+        2: "Calling",
+        3: "Sleeping",
+        4: "Drinking",
+        5: "Clapping",
+        6: "Dancing",
+        7: "Cycling",
+        8: "Calling",
+        9: "Laughing",
+        10: "Eating",
+        11: "Fighting",
+        12: "Listening_to_music",
+        13: "Running",
+        14: "Exting"
+    }
+    prediction = label_map[prediction]
+    return prediction
+
+@app.route('/', methods=["GET","POST"])
+def home():
+    if request.method=="POST":
+        image_file = request.files.get('image')
+        if image_file:
+            prediction = test_predict(image_file)
+            return render_template("index.html", result=prediction)
+        else:
+            return render_template("index.html", result="No image uploaded")
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(port=8080)
